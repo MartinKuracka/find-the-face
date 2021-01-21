f7209c1655b97ccdbef253f962db6877b31692d9
martin kuracka <m.kuracka@gmail.com>
2021-01-21 11:25:43 +0100

    style update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import Rank from './components/rank/rank';
 import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
+import ParicleParameters from './components/particles';
 import Particles from 'react-particles-js';
 
 function App() {
   return (
     <div className="App">
-      <Particles className='particles'/>
-      <Navigation />
-      <Logo />
+      <Particles className='particles' params={ParicleParameters}/>
+      <div className='spread pa2'>
+        <Logo />
+        <Navigation />        
+      </div>
       <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition />*/}
---
+++ src/components/particles.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const ParicleParameters = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  interactivity: {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    },
+    "modes": {
+      "grab": {
+        "distance": 100,
+        "line_linked": {
+          "opacity": 1
+        }
+      }
+    }  
+  }
+}
+
+export default ParicleParameters;
