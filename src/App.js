41177655f9d1be7330cbb2f90cb963c1ea2d0a82
martin kuracka <m.kuracka@gmail.com>
2021-01-24 22:38:00 +0100

    API

--- src/App.js
+++ src/App.js
@@ -4,55 +4,58 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import Rank from './components/rank/rank';
 import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticleParameters from './components/particles';
 import Particles from 'react-particles-js';
-import {ClarifaiStub, grpc} from "clarifai-nodejs-grpc";
+// import {ClarifaiStub, grpc} from "clarifai-nodejs-grpc";
 
-let app = new Clarifai.App({apiKey: '0d1a8485d84b452cb18e6d822cfc78e2'});
 
-const stub = ClarifaiStub.grpc();
+// let app = new Clarifai.App({apiKey: '0d1a8485d84b452cb18e6d822cfc78e2'});
+// const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 
-const metadata = new grpc.Metadata();
+// const stub = ClarifaiStub.grpc();
 
-metadata.set("authorization", "0d1a8485d84b452cb18e6d822cfc78e2");
+// const metadata = new grpc.Metadata();
+// metadata.set("authorization", "0d1a8485d84b452cb18e6d822cfc78e2");
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
     }
   };
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   };
 
   onButtonSubmit = () => {
-    console.log('click');
-    stub.PostModelOutputs( {
-          // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-          model_id: "aaa03c23b3724a16a56b629203edc62c",
-          inputs: [{data: {image: {url: "https://samples.clarifai.com/dog2.jpeg"}}}]
-      },
-      metadata,
-      (err, response) => {
-          if (err) {
-              console.log("Error: " + err);
-              return;
-          }
+    // console.log('click');
+    this.setState({imageUrl: this.state.input})
+    // stub.PostModelOutputs( {
+    //       model_id: "aaa03c23b3724a16a56b629203edc62c",
+    //       inputs: [{data: {image: {url: "https://samples.clarifai.com/dog2.jpeg"}}}]
+    //   },
+    //   metadata,
+    //   (err, response) => {
+    //       if (err) {
+    //           console.log("Error: " + err);
+    //           return;
+    //       }
 
-          if (response.status.code !== 10000) {
-              console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-              return;
-          }
+    //       if (response.status.code !== 10000) {
+    //           console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+    //           return;
+    //       }
 
-          console.log("Predicted concepts, with confidence values:")
-          for (const c of response.outputs[0].data.concepts) {
-              console.log(c.name + ": " + c.value);
-          }
-      }
-    );
+    //       console.log("Predicted concepts, with confidence values:")
+    //       for (const c of response.outputs[0].data.concepts) {
+    //           console.log(c.name + ": " + c.value);
+    //       }
+    //   }
+    // );
   };
 
   onLoginIn = () => {
@@ -71,7 +74,7 @@ class App extends React.Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}/>
-        {/*<FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     )
   };
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = ({onLoginIn}) => {
 	return (
 			<nav className='ma4 mt0'>
-				<p className='f3 link dim black underline pa3 pt5 pointer br2 shadow-2' onClick={onLoginIn} style={{ height: 150, width: 150 }}> Sign In</p>
+				<p className='f3 link dim black underline pa3 pt5 pointer br2 shadow-2' onClick={onLoginIn} style={{ height: 150, width: 150 }}> Sign Out</p>
 			</nav>
 	)
 }
