a750b94dcf88f7184997ffdd91dea0b178a91695
MartinKuracka <61198403+MartinKuracka@users.noreply.github.com>
2021-02-11 20:34:39 +0100

    update

--- src/components/rank/infobox.js
+++ src/components/rank/infobox.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './rank.css';
+// import './rank.css';
 
 const InfoBox = () => {
 	return (
-	  <div className='item-a f4 mt4'>
-		<h3 className='tc fw1-l'>
-			Find links to face images ending with <span className='yellow'>.jpg</span> or <span className='yellow'>.png </span>and click 'Detect'
+	  <div className='item-a f5 mt4'>
+		<h3 className='fw1-l'>
+			Find links to face images ending with <span className='dark-green'>.jpg</span> or <span className='dark-green'>.png </span>and click 'Detect'
 		</h3>
 	  </div>
 	);
--- src/components/rank/rank.js
+++ src/components/rank/rank.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './rank.css';
+// import './rank.css';
 import NoRank from './numberank';
 
 const Rank = ({name}) => {
 	return (
-	  <div className='item-b mt4'>
-			<div className='f4'>
-				<div className='item-b b--light-green bw2 br3 b--solid'>
-						{name}{', your current entry count is ...'}
-				</div>
-			</div>
+	  <div className='item-b f5 mt4'>
+		<div className='fw1-l'>
+			<span className='f3'>Hi {name},</span>
+			<br/>
+				{'your current entry count is ...'}
+			</div>			
       </div>  
 	);
 }
