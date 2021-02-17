ebf10c5ee061087b3aba316729fad88eb53e5bce
martin kuracka <m.kuracka@gmail.com>
2021-02-17 09:55:41 +0100

    update

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,8 +5,8 @@ import image from '../../img/hdrimg.png';
 const Header = () => {
 	return (
 		<div className=' header'>
-			<img className='fade-in-fwd head-img content-start tl' src={image} alt='header' width='15%' />
-			<h2 className='fade-in-fwd hd-margin w-25'>
+			<img className='slide-in-left head-img content-start tl' src={image} alt='header' width='15%' />
+			<h2 className='slide-in-left hd-margin w-25'>
 				Magic App to detect face in Your pictures
 			</h2>
 		</div>
