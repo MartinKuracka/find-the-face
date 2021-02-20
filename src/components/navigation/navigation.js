5b318327260ba53ef6369faf021576aadc6a68f6
MartinKuracka <61198403+MartinKuracka@users.noreply.github.com>
2021-02-20 10:16:49 +0100

    mobile

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -5,10 +5,10 @@ import Tilt from 'react-tilt';
 const Navigation = ({onRouteChange}) => {
 	return (
 		<div className='nav tc content-end'>
-			<Tilt className="Tilt br2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+			<Tilt className="Tilt br2" options={{ max : 55 }} >
 		  	<nav className='content-end '>
-					<p className='nav-box f3 link dim white underline pa3 pt3 pointer br2 shadow-2 b--light-green b--solid'
-					 	 onClick={() => onRouteChange('signin')} style={{ height: 100, width: 100 }}> Sign Out</p>
+					<p className='nav-size nav-box link dim white underline pa3 pt3 pointer br2 shadow-2 b--light-green b--solid'
+					 	 onClick={() => onRouteChange('signin')}> Sign Out</p>
 				</nav>
 			</Tilt>
 			
