7c1ad84b79465d925cba619f8d0fd160b04d617e
MartinKuracka <61198403+MartinKuracka@users.noreply.github.com>
2021-02-21 19:46:05 +0100

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import FootImage from '../../img/logo_footer.png'
 
 const Footer = () => {
     return(
-        <div className='footer center white'>
-            {'Designed with love by WebMarK'}           
+        <div className='w-100 flex center'>
+             <div className='footer white'>
+                <img className='f-i-size' src={FootImage}></img>
+                <br/>
+                {'Designed with love by WebMarK'}           
+             </div>
         </div>
+       
     )
 }
 
