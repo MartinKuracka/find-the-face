d0596103950c83a07d77d866c5ced279d64fbf8a
MartinKuracka <61198403+MartinKuracka@users.noreply.github.com>
2021-02-21 11:46:08 +0100

    animation fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,12 +5,23 @@ import HeaderBackground from '../../img/sample.png';
 
 const Header = () => {
 	return (
-		<div className=' header'>
-			<img className='header-background' src={HeaderBackground}/>
-			<img className='slide-in-left head-img content-start tl' src={image} alt='header'/>
-			<h2 className='slide-in-left hd-margin w-30'>
-				Magic App to detect faces in Your pictures
-			</h2>
+		<div>
+			{window.innerWidth > 600
+				? <div className=' header'>
+						<img className='header-background' src={HeaderBackground}/>
+						<img className='slide-in-left head-img content-start tl' src={image} alt='header'/>
+						<h2 className='slide-in-left hd-margin w-30'>
+							Magic App to detect faces in Your pictures
+						</h2>
+					</div>
+				: <div className=' header'>
+						<img className='header-background' src={HeaderBackground}/>
+						<img className='head-img content-start tl' src={image} alt='header'/>
+						<h2 className='hd-margin w-30'>
+							Magic App to detect faces in Your pictures
+						</h2>
+					</div>
+			}
 		</div>
 	)
 }
